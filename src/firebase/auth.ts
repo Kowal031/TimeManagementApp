b166112e1ef75c7f09aae4199131e5c36c414e30
Maciej <maciej.0.kowalczyk@euvic.pl>
2024-12-14 00:43:58 +0100

    Added basic firebase configuration and installed necessary packages

---
+++ src/firebase/auth.ts
@@ -0,0 +1,25 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import app from "./firebaseConfig";
+
+const auth = getAuth(app);
+
+export const registerUser = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const loginUser = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logoutUser = () => {
+  return signOut(auth);
+};
+
+export const getCurrentUser = () => {
+  return auth.currentUser;
+};
